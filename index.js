4bbc66fb26d9640ff5b25803373396db147cfebd
KHAIRI AHMAD (DCAZ/SLF-MY) <ahk1shm@bosch.com>
2021-09-22 15:32:08 +0800

    added some examples of strings

---
+++ index.js
@@ -0,0 +1,8 @@
+//strings must be wrapped in quotes
+//examples
+let firstname = "Ziggy";
+let msg = "Please do not feed the chimps!";
+//single quotes works as well but be consistent
+let animal = 'Dumbo Octopus';
+//string inside a string
+let myName = "'T.S.' Ahmad Khairi";
