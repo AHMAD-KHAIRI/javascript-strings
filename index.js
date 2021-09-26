171c384705abd993e00b350b60141c3992902bc5
KHAIRI AHMAD (DCAZ/SLF-MY) <ahk1shm@bosch.com>
2021-09-26 12:55:20 +0800

    Added a string method exercise

--- index.js
+++ index.js
@@ -48,6 +48,18 @@ greeting.replace('hello', 'hi'); //outputs "  hi hello helloooo!!   "
 // .repeat(count) method
 greeting.repeat(2); //outputs "  hello hello helloooo!!     hello hello helloooo!!   "
 
+//exercise
+/*
+Goal is to use string methods on word, so that you end up with the string 'beard'.
+1. Use a string method to extract the "board" part of "skateboard" (using the slice method)
+2. Replace the "o" in "board" with an "e" (using the replace method)
+3. Save the result in a variable called facialHair
+*/
+const word = "skateboard"; //Don't change this line!
+
+// YOUR CODE BELOW THIS LINE:
+let facialHair = word.slice(5).replace('o', 'e');   //outputs 'beard'
+
 
 
 
