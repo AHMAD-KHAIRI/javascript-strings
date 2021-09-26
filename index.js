cfa58292b35f3a5e2a85e9e89b69d1cdc41f450a
KHAIRI AHMAD (DCAZ/SLF-MY) <ahk1shm@bosch.com>
2021-09-26 12:25:14 +0800

    added some string methods such as trim(),toUpperCase(),indexOf(), etc

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 //strings must be wrapped in quotes
 //examples
-let firstname = "Ziggy";
-let msg = "Please do not feed the chimps!";
+let firstname = 'Ziggy';
+let msg = 'Please do not feed the chimps!';
 //single quotes works as well but be consistent
 let animal = 'Dumbo Octopus';
 //string inside a string
-let myFirstName = "'Mr.' Ahmad Khairi";
+let myFirstName = '"Mr." Ahmad Khairi';
 
 //strings are indexed. Each character has a corresponding index (a positional number)
 //example: HELLO !
 //         0123456
 // H has an index of 0, E has an index of 1, ..., space has an index of 5, etc
 //example
-let myLastName = "Hamzah";
+let myLastName = 'Hamzah';
 //to access the string located inside the index:
-myLastName[0]; // = "H"
-myLastName[1]; // = "a"
-myLastName[2]; // = "m"
+myLastName[0]; // outputs 'H'
+myLastName[1]; // outputs 'a'
+myLastName[2]; // outputs 'm'
 //and so on...
 
-//to know the length of the string
+//to know the length of the string using .length() method
 //example
 myName.length; //
 //we can combine strings (or known as concatenation-the action of linking things together in a series.)
@@ -27,10 +27,28 @@ myFirstName + myLastName; // = "'Mr.' Ahmad Khairi Hamzah"
 
 //string methods
 //syntax: string.method()
-//example:
-let greeting = "  hello hello helloooo!!   ";
+//examples:
+let greeting = '  hello hello helloooo!!   ';
+//             '0123456789
+// .trim() method
 greeting.trim();    //results: "hello hello helloooo!!""   (notice there's no whitespace)
+// .toUpperCase() method
 greeting.toUpperCase(); //results:"  HELLO HELLO HELLOOOO!!   "
-//combine more than 1 method: string.method1().method2()
+//it's possible to combine more than 1 method: string.method1().method2()
 greeting.trim().toUpperCase(); //results: "HELLO HELLO HELLOOOO!!"
+// .indexOf(arg) method
+greeting.indexOf('hello');  //outputs 2 --> refer line 32 for index num
+greeting.indexOf('o');  //outputs 6
+// .slice() method
+//syntax .slice(beginIndex)
+greeting.slice(2);  //outputs 'hello hello helloooo!!   '
+greeting.slice(2, 7); //outputs 'hello'
+// .replace(regexp, newSubstr) method
+greeting.replace('hello', 'hi'); //outputs "  hi hello helloooo!!   "
+// .repeat(count) method
+greeting.repeat(2); //outputs "  hello hello helloooo!!     hello hello helloooo!!   "
+
+
+
+
 
